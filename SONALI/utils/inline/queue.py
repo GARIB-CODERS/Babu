6ebad12faff22318722c4689cd048d4fc23d6401
GARIB-CODERS <landrepo541@gmail.com>
2024-11-29 05:55:29 +0530

    Update queue.py

--- SONALI/utils/inline/queue.py
+++ SONALI/utils/inline/queue.py
@@ -73,10 +73,10 @@ def aq_markup(_, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/mr_aman_077"
+                text="⛦ ᴏᴡɴᴇʀ ⛦", url=f"https://t.me/ll_THUNDER_lll"
             ),
             InlineKeyboardButton(
-                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/flirtingworldchatting"
+                text="🥀 ꜱᴜᴘᴘᴏʀᴛ 🥀", url=f"https://t.me/CRAZY_OP_02"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
